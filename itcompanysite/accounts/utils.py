cb52eec1426d08e6d438ee11b3a4d6faf97db475
Nikita Ershov <ershov-n-a1009@mail.ru>
2024-09-21 14:54:28 +0500

    role functional

--- itcompanysite/accounts/utils.py
+++ itcompanysite/accounts/utils.py
@@ -1,9 +1,21 @@
 from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import redirect
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from django.core.mail import EmailMessage
 from django.contrib.auth.tokens import default_token_generator as token_generator
+
+
+def role_required(role):
+    def decorator(view_func):
+        def _wrapped_view(request, *args, **kwargs):
+            if request.user.role == role:
+                return view_func(request, *args, **kwargs)
+            return redirect('no_permission')  # или любое другое действие
+        return _wrapped_view
+    return decorator
+
 
 def send_email_for_verify(request, user):
     current_site = get_current_site(request)
