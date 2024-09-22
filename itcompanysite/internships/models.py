5be1f43973e7e091e926ff0ddc0a2e7de3be5c43
Nikita Ershov <ershov-n-a1009@mail.ru>
2024-09-22 05:49:44 +0500

    стажировка страницы

--- itcompanysite/internships/models.py
+++ itcompanysite/internships/models.py
@@ -3,17 +3,19 @@ from companies.models import Company, CompanyAddress
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-class Internship(models.Model):
 
-    all_places = models.IntegerField('all_places')
-    available_places = models.IntegerField('available_places')
-    time_type_work = models.TextField('time_type_work')
-    date_start = models.DateField('date_start')
-    date_end = models.DateField('date_end')
-    salary = models.IntegerField('salary')
-    description = models.TextField('description', default="")
-    company = models.ForeignKey(Company, on_delete=models.DO_NOTHING,  default='')
-    company_address = models.ForeignKey(CompanyAddress, on_delete=models.DO_NOTHING, default='')
+
+class Internship(models.Model):
+    all_places = models.IntegerField('Всего мест')
+    available_places = models.IntegerField('Доступных мест')
+    time_type_work = models.TextField('Тип рабочего времени')
+    date_start = models.DateField('Дата начала')
+    date_end = models.DateField('Дата окончания')
+    salary = models.IntegerField('Зарплата')
+    description = models.TextField('Описание', default="")
+    company = models.ForeignKey(Company, on_delete=models.DO_NOTHING, default='', verbose_name='Компания')
+    company_address = models.ForeignKey(CompanyAddress, on_delete=models.DO_NOTHING, default='', verbose_name='Адрес компании')
+
 
 
 class Specialization(models.Model):
--- itcompanysite/internships/views.py
+++ itcompanysite/internships/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import InternshipCreationForm
 from .models import Internship
+from django.http import Http404
+
+
+def get_object_or_404(model, **kwargs):
+    try:
+        return model.objects.get(**kwargs)
+    except model.DoesNotExist:
+        raise Http404(f"{model._meta.verbose_name} не найден.")
 
 
 @login_required
@@ -20,4 +28,12 @@ def create_internship(request):
 def internships_list(request):
     internships = Internship.objects.all()
     return render(request, 'internships/internships_list.html', {'internships': internships})
+
+
+def internship_detail(request, internship_id):
+    # Получаем стажировку или возвращаем 404, если не найдена
+    internship = get_object_or_404(Internship, pk=internship_id)
+
+    # Рендерим шаблон с деталями стажировки
+    return render(request, 'internships/internship_detail.html', {'internship': internship})
 
